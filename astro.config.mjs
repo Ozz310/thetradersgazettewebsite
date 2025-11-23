624cdb3c2b685696702b72e844942b3b3df2106e
Mohammad Osama <mohammadosama310@gmail.com>
2025-11-23 17:47:02 +0500

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  // IMPORTANT: The base path is set to '/' because the website will be on a custom domain (www.thetradersgazette.com),
+  // not a subdirectory like yourname.github.io/repo-name.
+  base: '/',
+  site: 'https://www.thetradersgazette.com/',
+  integrations: [tailwind(), react()],
+  // Essential for static deployment on GitHub Pages
+  output: 'static'
+});
